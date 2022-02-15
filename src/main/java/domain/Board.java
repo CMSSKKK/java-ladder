4b1426c691c734b7f8c9e964420104a4ca9a9b5f
Minseok-Choi <cms05041@gmail.com>
2022-02-15 18:05:50 +0900

    refactor(Board) : frame을 배열에서 list로 수정

--- src/main/java/domain/Board.java
+++ src/main/java/domain/Board.java
@@ -1,6 +1,8 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static Util.Valid.checkPlayers;
 
@@ -8,50 +10,53 @@ public class Board {
 
     private static final String VERTICAL = "|";
 
-
-    private final String[][] frame;
+    private final int players;
+    private final int height;
+    private final List<List<String>> frame;
     private final Line line;
 
     public Board(int players, int height) {
+        this.players = players;
+        this.height = height;
         checkPlayers(players);
-        frame = new String[height][(players*2)-1];
+        frame = new ArrayList<>();
         line = new Line();
     }
 
     public int countPlayers() {
-        return frame[0].length/2+1;
+        return players;
     }
 
     public int getLadderHeight() {
-        return frame.length;
+        return height;
     }
 
     public void generateFrame() {
-        for (int row = 0; row < frame.length ; row++) {
-            draw(row);
+        for (int row = 0; row < height; row++) {
+            frame.add(draw(row));
         }
     }
 
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
-        for (String[] strings : frame) {
-            stringBuilder.append(Arrays.toString(strings).replaceAll("\\[|\\]|,","")).append('\n');
-        }
+
         return stringBuilder.toString();
     }
 
-    private void draw(int row) {
-        for (int col = 0; col < getLadderHeight() ; col++) {
-            frame[row][col] = drawLine(col);
+    private List<String> draw(int row) {
+        List<String> floor = new ArrayList<>();
+        for (int col = 0; col < players*2-1 ; col++) {
+            floor.add(drawLine(col));
         }
+        return floor;
     }
 
     private String drawLine(int col) {
         if(col % 2 == 0) {
             return VERTICAL;
         }
-        return line.generateRandomLine();
+        return line.generatRandomLine();
     }
 }
 

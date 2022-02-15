609ca96e0586296d935be34cc601f4a440336812
Minseok-Choi <cms05041@gmail.com>
2022-02-15 15:52:55 +0900

    feat(Valid) : 입력 검증을 위한 checkNames(), checkNameLength() 메서드 구현, 상수 수정

--- src/main/java/Util/Valid.java
+++ src/main/java/Util/Valid.java
@@ -3,11 +3,25 @@ package Util;
 public class Valid {
 
     private static final int MIN_PLAYER = 2;
-    private static final String PLAYER_ERROR_MESSAGE = "사람 수는 2명 이상이어야 합니다.";
+    private static final String PLAYER_COUNT_ERROR_MESSAGE = "player의 수는 2명 이상이어야 합니다.";
+    private static final String PLAYER_NAME_ERROR_MESSAGE = "player의 이름은 5글자까지 가능합니다.";
 
     public static void checkPlayers(int count) {
         if(count < MIN_PLAYER) {
-            throw new IllegalArgumentException(PLAYER_ERROR_MESSAGE);
+            throw new IllegalArgumentException(PLAYER_COUNT_ERROR_MESSAGE);
         }
+    }
+    // 도메인 구현 후 input검증을 정규식으로 리팩토링 해봐야 함
+    public static void checkNames(String input) {
+        if(!input.contains(",")) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static boolean checkNameLength(String name) {
+        if(name.length() > 5) {
+            throw new IllegalArgumentException(PLAYER_NAME_ERROR_MESSAGE);
+        }
+        return true;
     }
 }

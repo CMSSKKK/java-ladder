1a02dc1c9f5f26f823984c1d0b043daa2edeff11
Minseok-Choi <cms05041@gmail.com>
2022-02-15 15:44:38 +0900

    feat : 플레이어의 이름을 담을 일급 컬렉션 Names 생성

---
+++ src/main/java/domain/Names.java
@@ -0,0 +1,18 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Names {
+
+    private List<String> names;
+
+    public Names(String strNames) {
+        this.names = new ArrayList<>();
+        generateNames(strNames);
+    }
+
+    public void generateNames(String strNames) {
+
+    }
+}

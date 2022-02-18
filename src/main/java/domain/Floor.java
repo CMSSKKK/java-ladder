df17a39aaf09aac3b44f0152b5990af103828dc4
Minseok-Choi <cms05041@gmail.com>
2022-02-18 14:59:52 +0900

    refactor : Floor의 리스트에 저장되는 값을 Boolean으로 수정
    generateFloor의 내부 메서드 boolean에 맞게 다 수정
    toText메서드를 통해서 toString을 출력예시와 같이 변환함

--- src/main/java/domain/Floor.java
+++ src/main/java/domain/Floor.java
@@ -2,57 +2,55 @@ package domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 
 public class Floor {
 
-    private static final String VERTICAL = "|";
-    private static final String LINE = "-----";
-    private static final String BLANK = "     ";
+    private static final String START_VERTICAL = "    |";
+    private static final String LINE = "-----|";
+    private static final String BLANK = "     |";
     private static final Random random = new Random();
 
-    private final List<String> floor;
+    private final List<Boolean> floor;
 
     public Floor(int size) {
         this.floor = generateFloor(size);
     }
 
-    private List<String> generateFloor(int size) {
-        List<String> parts = new ArrayList<>();
-        for (int i = 0; i < size ; i++) {
+    private List<Boolean> generateFloor(int size) {
+        List<Boolean> parts = new ArrayList<>();
+        parts.add(null);
+        for (int i = 1; i <= size ; i++) {
             parts.add(drawPart(parts,i));
         }
+        parts.add(null);
         return parts;
     }
 
-    private String generateLine(boolean b) {
-        if (b) {
+    private boolean drawPart(List<Boolean> floor, int index) {
+        if(floor.get(index-1)) {
+          return false;
+        }
+        return drawRandom();
+    }
+
+    private boolean drawRandom() {
+        return random.nextBoolean();
+    }
+
+    private String toText(boolean b) {
+        if(b) {
             return LINE;
         }
         return BLANK;
     }
 
-    private String generateRandomLine(List<String> list, int index) {
-        if (index == 1) {
-            return generateLine(random.nextBoolean());
-        }
-        if (list.get(index - 2).equals(BLANK)) {
-            return generateLine(random.nextBoolean());
-        }
-        return BLANK;
-    }
-
-    private String drawPart(List<String> list, int index) {
-        if(index % 2 == 0) {
-            return VERTICAL;
-        }
-        return generateRandomLine(list,index);
-    }
-
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
-        floor.forEach(stringBuilder::append);
-        return stringBuilder.append('\n').toString();
+        stringBuilder.append(START_VERTICAL);
+        floor.stream().filter(Objects::nonNull).map(this::toText).forEach(stringBuilder::append);
+        return stringBuilder.toString();
     }
 }

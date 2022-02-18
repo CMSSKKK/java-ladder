e1241d51347e6c992371074c145cf11c73239cc0
Minseok-Choi <cms05041@gmail.com>
2022-02-18 18:59:12 +0900

    feat : 5단계 실행 결과 입력,출력, 로직 구현

--- src/main/java/GameController.java
+++ src/main/java/GameController.java
@@ -1,14 +1,38 @@
 import View.InputView;
 import View.OutputView;
-import domain.Ladder;
 import domain.LadderGame;
 
 public class GameController {
 
-    public void run() {
+    private static final String QUIT_COMMAND = "춘식이";
+    private static final String ALL_COMMNAD = "all";
+
+    public void init() {
         LadderGame ladderGame = InputView.askSettingLadder();
-        InputView.close();
         OutputView.showLadder(ladderGame);
+        run(ladderGame);
+        InputView.close();
     }
+
+    private void run(LadderGame ladderGame) {
+        String target;
+        while (!(target = InputView.askPlayerForResult()).equals(QUIT_COMMAND)) {
+            showResult(ladderGame,target);
+        }
+    }
+
+    private void showResult(LadderGame ladderGame, String command) {
+        if(command.equals(ALL_COMMNAD)) {
+            OutputView.showAllResult(ladderGame);
+            return;
+        }
+        try {
+            OutputView.showTargetResult(ladderGame,command);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            run(ladderGame);
+        }
+    }
+
 
 }
